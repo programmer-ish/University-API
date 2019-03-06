6e7e1639134947feb22924789a09a94a3982a7b2
ishita <ishita.srivastava@st.niituniversity.in>
2019-03-06 15:18:38 +0530

    Added classes router

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+const bodyParser=require("body-parser");
 const express=require("express");
-const app=express();
 
+const db=require("./app/database");
+const classesRouter=require("./app/classesRouter");
 const studentRouter=require("./app/studentRouter");
 
+//Server setup
+const app=express();
 const port=4000;
+db();
 
 app.use(bodyParser.urlencoded({   extended: true })); 
 app.use(bodyParser.json());
 
-app.use("/student",studentRouter); //middleware student
+app.use("/classes",classesRouter); //middleware student
+app.use("/students",studentRouter); //middleware student
 
 const server=app.listen(port,()=>{
     console.log("Server is running");
