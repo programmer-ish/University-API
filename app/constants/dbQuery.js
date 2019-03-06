fe2044f901d10cd4ca9903cb77422a3389f5e543
ishita <ishita.srivastava@st.niituniversity.in>
2019-03-06 22:45:38 +0530

    Updated GET_CLASSES_OF_STUDENT to include professor's name

--- app/constants/dbQuery.js
+++ app/constants/dbQuery.js
@@ -3,7 +3,9 @@ module.exports={
     POST_CLASS:"INSERT INTO SemesterClass(title) VALUES($1)",
     GET_STUDENTS:"select * from Student",
     GET_STUDENT_BY_ID:"select * from Student where roll_no=",
-    GET_CLASSES_OF_STUDENT:"select * from ClassStudent inner join SemesterClass on ClassStudent.classId=SemesterClass.id where ClassStudent.studentId=",
+    GET_CLASSES_OF_STUDENT:"select * from ClassStudent inner join SemesterClass on ClassStudent.classId=SemesterClass.id \
+    inner join ClassProfessor on ClassProfessor.classId=ClassStudent.classId \
+    inner join Professor on ClassProfessor.professorId=Professor.university_staff_no where ClassStudent.studentId=",
     POST_STUDENT:"INSERT INTO Student(name, admissionDate) VALUES($1, $2)",
     PATCH_STUDENT:"UPDATE Student SET name=$1 WHERE roll_no=$2",
     DELETE_STUDENT:"UPDATE Student SET active=FALSE WHERE roll_no=",
--- app/studentRouter.js
+++ app/studentRouter.js
@@ -1,67 +1,67 @@
 const express = require("express");
-const {Client}=require('pg');
+const { Client } = require('pg');
 
 const QUERY_CONST = require("./constants/dbQuery");
-const RESPONSE_CONST=require("./constants/response");
+const RESPONSE_CONST = require("./constants/response");
 
 //DB connection
-const client =new Client("postgres://ehhcmqsb:xcBCgsMlrcyqlUsU0mPL6aljm3B2UBxV@isilo.db.elephantsql.com:5432/ehhcmqsb");
+const client = new Client("postgres://ehhcmqsb:xcBCgsMlrcyqlUsU0mPL6aljm3B2UBxV@isilo.db.elephantsql.com:5432/ehhcmqsb");
 
 client.connect()
-.then(() => console.log("Connected to db successfully"))
-.catch(e => console.log(e)) 
+    .then(() => console.log("Connected to db successfully"))
+    .catch(e => console.log(e))
 
 //API calls
 const router = express.Router();
 
-router.get("/", (req, res)=> {
-    client.query(QUERY_CONST.GET_STUDENTS)
-    .then(result => {return res.send(result.rows)})
-    .catch(e =>console.error(e.stack))
+
+
+
+router.get("/:id", (req, res) => {
+    client.query(QUERY_CONST.GET_STUDENT_BY_ID + req.params.id)
+        .then(result => { return res.send(result.rows) })
+        .catch(e => console.error(e.stack))
 });
 
-router.get("/:id", (req, res)=> {
-    client.query(QUERY_CONST.GET_STUDENT_BY_ID+req.params.id)
-    .then(result => {return res.send(result.rows)})
-    .catch(e =>console.error(e.stack))
+router.get("/:id/classes", (req, res) => {
+    client.query(QUERY_CONST.GET_CLASSES_OF_STUDENT + req.params.id)
+        .then(result => {
+            (result.rowCount != 0 ? res.send(result.rows) : res.status(404).json({
+                message: RESPONSE_CONST.HTTP_404
+            }))
+        })
+        .catch(e => {res.send(e.stack)})
 });
 
-router.get("/:id/classes", (req, res)=> {
-    client.query(QUERY_CONST.GET_CLASSES_OF_STUDENT+req.params.id)
-    .then(result => {return res.send(result.rows)})
-    .catch(e =>console.error(e.stack))
+router.post('/', (req, res) => {
+    const query = QUERY_CONST.POST_STUDENT
+    const values = [req.body.name, req.body.admissionDate]
+
+    client.query(query, values)
+        .then(() =>
+            res.status(201).json({
+                message: RESPONSE_CONST.HTTP_201
+            })
+        )
+        .catch(e => res.status(400).json({
+            message: RESPONSE_CONST.HTTP_400
+        }))
 });
 
-router.post('/',(req,res)=>{
-    const query = QUERY_CONST.POST_STUDENTS
-    const values = [req.body.name,req.body.admissionDate]
+router.patch("/:id", (req, res) => {
+    const query = QUERY_CONST.PATCH_STUDENT
+    const values = [req.query.name, req.params.id]
 
-   client.query(query,values)
-   .then(() =>
-       res.status(201).json({
-           message: RESPONSE_CONST.HTTP_201
-       })
-   )
-   .catch(e =>res.status(400).json({
-       message: RESPONSE_CONST.HTTP_400
-   })) 
+    client.query(query, values)
+        .then(() => res.status(200))
+        .catch(e => res.status(400))
 });
 
-router.patch("/:id",(req,res)=>{
-    const query = QUERY_CONST.PATCH_STUDENTS
-    const values = [req.query.name,req.params.id]
-
-   client.query(query,values)
-   .then(() =>res.status(200))
-   .catch(e =>res.status(400)) 
-});
-
-router.delete("/:id",(req,res)=>{
-    const query = QUERY_CONST.DELETE_STUDENTS+req.params.id
+router.delete("/:id", (req, res) => {
+    const query = QUERY_CONST.DELETE_STUDENT + req.params.id
     client.query(query)
-   .then(() =>res.status(200))
-   .catch(e =>res.status(400)) 
+        .then(() => res.status(200))
+        .catch(e => res.status(400))
 });
 
-
-module.exports=router;
+module.exports = router;
