5c10ef54a5b8b9aa2ea555ddc0040db52ae5064b
ishita <ishita.srivastava@st.niituniversity.in>
2019-03-06 21:37:27 +0530

    Added GET_PROFESSORS

--- app/constants/dbQuery.js
+++ app/constants/dbQuery.js
@@ -7,5 +7,6 @@ module.exports={
     POST_STUDENT:"INSERT INTO Student(name, admissionDate) VALUES($1, $2)",
     PATCH_STUDENT:"UPDATE Student SET name=$1 WHERE roll_no=$2",
     DELETE_STUDENT:"UPDATE Student SET active=FALSE WHERE roll_no=",
-    POST_STUDENTS_TO_CLASS:"INSERT INTO ClassStudent(classId,studentId) VALUES"
+    POST_STUDENTS_TO_CLASS:"INSERT INTO ClassStudent(classId,studentId) VALUES",
+    GET_PROFESSORS:"select * from Professor"
 }
---
+++ app/professorRouter.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const {Client}=require('pg');
+
+const QUERY_CONST = require("./constants/dbQuery");
+const RESPONSE_CONST=require("./constants/response");
+
+//DB connection
+const client =new Client("postgres://ehhcmqsb:xcBCgsMlrcyqlUsU0mPL6aljm3B2UBxV@isilo.db.elephantsql.com:5432/ehhcmqsb");
+
+client.connect()
+.then(() => console.log("Connected to db successfully"))
+.catch(e => console.log(e)) 
+
+
+//API calls
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    client.query(QUERY_CONST.GET_PROFESSORS)
+    .then(result => {return res.send(result.rows)})
+    .catch(e =>console.error(e.stack))
+});
