1498036850b8fd4d8b4f78029df6b6a092ba47b0
ishita <ishita.srivastava@st.niituniversity.in>
2019-03-06 16:51:16 +0530

    Added POST_STUDENTS_TO_CLASS

--- app/classesRouter.js
+++ app/classesRouter.js
@@ -22,7 +22,7 @@ router.get("/", (req, res) => {
 });
 
 router.post('/',(req,res)=>{
-    const query = QUERY_CONST.POST_CLASSES
+    const query = QUERY_CONST.POST_CLASS
     const values = [req.query.title]
 
     client.query(query,values)
@@ -36,4 +36,24 @@ router.post('/',(req,res)=>{
     })) 
 });
 
+router.post("/:id/students", (req, res)=> {
+    const id=req.params.id;
+    const query = QUERY_CONST.POST_STUDENTS_TO_CLASS
+    let value = '';
+    for(var val of req.body.students){
+        value += '('+id+','+val+'),'
+    }
+    value=value.substring(0,value.length-1);
+    value=query+value+';';
+    client.query(value)
+    .then(() =>
+        res.status(201).json({
+            message: RESPONSE_CONST.HTTP_201
+        })
+    )
+    .catch(e =>res.status(400).json({
+        message: RESPONSE_CONST.HTTP_400
+    })) 
+
+});
 module.exports=router;
--- app/constants/dbQuery.js
+++ app/constants/dbQuery.js
@@ -1,10 +1,11 @@
 module.exports={
     GET_CLASSES:"select * from SemesterClass",
-    POST_CLASSES:"INSERT INTO SemesterClass(title) VALUES($1)",
+    POST_CLASS:"INSERT INTO SemesterClass(title) VALUES($1)",
     GET_STUDENTS:"select * from Students",
     GET_STUDENT_BY_ID:"select * from Student where roll_no=",
     GET_CLASSES_OF_STUDENT:"select * from ClassStudent inner join SemesterClass on ClassStudent.classId=SemesterClass.id where ClassStudent.studentId=",
-    POST_STUDENTS:"INSERT INTO Student(name, admissionDate) VALUES($1, $2)",
-    PATCH_STUDENTS:"UPDATE Student SET name=$1 WHERE roll_no=$2",
-    DELETE_STUDENTS:"UPDATE Student SET active=FALSE WHERE roll_no="
+    POST_STUDENT:"INSERT INTO Student(name, admissionDate) VALUES($1, $2)",
+    PATCH_STUDENT:"UPDATE Student SET name=$1 WHERE roll_no=$2",
+    DELETE_STUDENT:"UPDATE Student SET active=FALSE WHERE roll_no=",
+    POST_STUDENTS_TO_CLASS:"INSERT INTO ClassStudent(classId,studentId) VALUES"
 }
